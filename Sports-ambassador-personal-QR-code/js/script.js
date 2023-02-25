989b676a41798c0c45fa353a5b35dcffbb904a99
yanhun1234 <122240912+yanhun1234@users.noreply.github.com>
2023-02-25 08:46:17 +0800

    Update script.js

--- Sports-ambassador-personal-QR-code/js/script.js
+++ Sports-ambassador-personal-QR-code/js/script.js
@@ -1,1 +1,12 @@
-
+localStorage.a_1=0
+localStorage.a_2=0
+localStorage.a_3=0
+localStorage.a_4=0
+localStorage.a_5=0
+localStorage.a_6=0
+localStorage.a_7=0
+localStorage.a_8=0
+localStorage.a_9=0
+localStorage.a_10=0
+localStorage.a_11=0
+localStorage.a_12=0
