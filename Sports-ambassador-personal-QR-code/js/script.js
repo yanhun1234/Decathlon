4f6d831183a1fb9a6c55c5358bbb9f054518fcc7
yanhun1234 <122240912+yanhun1234@users.noreply.github.com>
2023-02-25 09:01:56 +0800

    Update script.js

--- Sports-ambassador-personal-QR-code/js/script.js
+++ Sports-ambassador-personal-QR-code/js/script.js
@@ -13,3 +13,51 @@ if (typeof(Storage) !== "undefined") {
   localStorage.a_12=0
 } else {
 }
+function a_1()
+{
+  localStorage.a_1=1
+}
+function a_2()
+{
+  localStorage.a_2=1
+}
+function a_3()
+{
+  localStorage.a_3=1
+}
+function a_4()
+{
+  localStorage.a_4=1
+}
+function a_5()
+{
+  localStorage.a_5=1
+}
+function a_6()
+{
+  localStorage.a_6=1
+}
+function a_7()
+{
+  localStorage.a_7=1
+}
+function a_8()
+{
+  localStorage.a_8=1
+}
+function a_9()
+{
+  localStorage.a_9=1
+}
+function a_10()
+{
+  localStorage.a_10=1
+}
+function a_11()
+{
+  localStorage.a_11=1
+}
+function a_12()
+{
+  localStorage.a_12=1
+}
